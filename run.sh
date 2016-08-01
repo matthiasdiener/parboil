401e64b8c212db0718904a6d0c20ebdd86a752e3
Matthias Diener <mdiener@inf.ufrgs.br>
2016-08-01 12:09:43 -0300

    clean up datasets

--- run.sh
+++ run.sh
@@ -5,7 +5,14 @@ set -o errexit -o nounset -o posix -o pipefail
 bm="bfs cutcp histo lbm mri-gridding mri-q sad sgemm spmv stencil tpacf"
 
 for b in $bm; do
-	echo $b
+	echo -n "making $b: "
 	./parboil compile $b omp_base
 done
 
+echo
+
+for b in $bm; do
+	echo "running $b"
+	./parboil run --no-check $b omp_base large
+	echo
+done
