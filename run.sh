4ee72ee42cd1d643fab74db33d41c593c4913157
Matthias Diener <mdiener@inf.ufrgs.br>
2016-08-01 13:01:22 -0300

    add run stats

--- run.sh
+++ run.sh
@@ -2,17 +2,17 @@
 
 set -o errexit -o nounset -o posix -o pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# 11 in total
 bm="bfs cutcp histo lbm mri-gridding mri-q sad sgemm spmv stencil tpacf"
 
-for b in $bm; do
-	echo -n "making $b: "
-	./parboil compile $b omp_base
-done
-
-echo
+mkdir -p run
 
 for b in $bm; do
 	echo "running $b"
-	./parboil run --no-check $b omp_base large
+	export GOMP_CPU_AFFINITY=0-256 KMP_AFFINITY=explicit,verbose,proclist=0-256
+	sudo -E perf stat -A -a -e instructions,cache-misses,cache-references,cycles ./parboil run --no-check $b omp_base large |& tee run/$b.txt
 	echo
 done
