c49c51a7cabbfa2eebf777c5c2f2a2de889ee02a
Matthias Diener <mdiener@inf.ufrgs.br>
2016-08-22 17:17:38 -0300

    better run command

--- run.sh
+++ run.sh
@@ -8,11 +8,18 @@ cd $DIR
 # 10 in total
 bm="bfs cutcp histo lbm mri-gridding mri-q sgemm spmv stencil tpacf"
 
-mkdir -p run
+OUTDIR=~/Dropbox/results/$(hostname)/$(basename $DIR)/
+mkdir -p $OUTDIR
+
+exe() { echo "++ $@" |& tee -a $OUTDIR/$b.txt ;  if [[ "$1" == "export" ]]; then echo X$1; $@ ; fi ;  $@ |& tee -a $OUTDIR/$b.txt ; }
 
 for b in $bm; do
-	echo "running $b"
-	export GOMP_CPU_AFFINITY=0-256 KMP_AFFINITY=explicit,verbose,proclist=0-256
-	sudo -E perf stat -A -a -e instructions,cache-misses,cache-references,cycles ./parboil run --no-check $b omp_base large |& tee run/$b.txt
+	echo -n > $OUTDIR/$b.txt #clean output file
+	exe echo "running $b"
+	exe date
+	exe uname -a
+	[[ -x /usr/bin/hwloc-ls ]] && exe hwloc-ls
+	exe export GOMP_CPU_AFFINITY=0-1024 KMP_AFFINITY=explicit,verbose,proclist=[0-1024]
+	exe sudo -E perf stat -A -a -e instructions,cache-misses,cache-references,cycles ./parboil run --no-check $b omp_base large
 	echo
 done
